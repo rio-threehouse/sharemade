069de94a90351cdd8123abc02f453c06a5bf3799
Rio Sanka <threehouse.second@gmail.com>
2019-08-14 15:38:21 +0900

    修正　文字数　レイアウト

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,8 +1,8 @@
 class Result < ApplicationRecord
   belongs_to :user
 
-  validates :subtitle, length: { maximum: 70 }, presence: true
-  validates :title, length: { maximum: 20 }, presence: true
+  validates :subtitle, length: { maximum: 150 }, presence: true
+  validates :title, length: { maximum: 100 }, presence: true
   validates :detail, length: { maximum: 65535 }
   validates :url, length: { maximum: 65535 }, presence: true
   validates :github, length: { maximum: 65535 }
