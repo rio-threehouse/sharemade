496dee15378820b842c5d361041dbde547a7888b
Rio Sanka <threehouse.second@gmail.com>
2019-09-02 10:29:13 +0900

    heroku error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: "AWS",
-      AWS_access_key_id: Rails.application.secrets.AWS_access_key_id,
-      AWS_secret_key:   Rails.application.secrets.AWS_secret_key,
+      AWS_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      AWS_secret_key:   ENV["AWS_SECRET_ID"],
       region: 'ap-northeast-1'
     }
 
