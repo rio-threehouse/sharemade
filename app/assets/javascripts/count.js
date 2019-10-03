c7cb809610989a377cfb86b91573420bba1b4e4f
Rio Sanka <threehouse.second@gmail.com>
2019-10-03 21:55:48 +0900

    文字数カウント 改行時のカウント数を2文字分にする

--- app/assets/javascripts/count.js
+++ app/assets/javascripts/count.js
@@ -1,16 +1,16 @@
 // jquery書きはじめ
 $(function (){
   // 処理
-  var count = $(".js-text").text().length;
+  var count = $(".js-text").text().replace(/\r?\n/g, "改行").length;
   var now_count = 150 - count;
   $(".js-text-count").text( "のこり" + now_count + "文字");
 
   $(".js-text").on("keyup", function() {
-    var count = $(this).val().length;
+    var count = $(this).val().replace(/\r?\n/g, "改行").length;
     var now_count = 150 - count;
     
-    
     // バリデーションの値を変数に入れたい 最大●文字、のところにクラスを当てて、textを取得して変数に入れる
+    // 問題 改行はにも自分プラスする！
     if (count > 150) {
       $(".js-text-count").css("color","red");
       $(".js-text-count").text( now_count + "文字オーバー");
