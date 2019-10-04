de90c1e2597a26482ad3e4e4b30e86363dec27f0
Rio Sanka <threehouse.second@gmail.com>
2019-10-04 19:50:08 +0900

    作品登録フォーム　文字カウント追加

--- app/assets/javascripts/count.js
+++ app/assets/javascripts/count.js
@@ -1,20 +1,19 @@
 // jquery書きはじめ
 $(function (){
   // 処理
-  var count = $(".js-text").text().replace(/\r?\n/g, "改行").length;
-  var now_count = 150 - count;
-  if (count > 150) {
+  var max_text = $('.js-max-text').text()
+  var count = $(".js-text").text().replace(/\n/g, "改行").length;
+  var now_count = max_text - count;
+  if (count > max_text) {
     $(".js-text-count").css("color","red");
   }
   $(".js-text-count").text( "残り" + now_count + "文字");
 
   $(".js-text").on("keyup", function() {
-    var count = $(this).val().replace(/\r?\n/g, "改行").length;
-    var now_count = 150 - count;
-    
-    // バリデーションの値を変数に入れたい 最大●文字、のところにクラスを当てて、textを取得して変数に入れる
-    // 問題 改行はにも自分プラスする！
-    if (count > 150) {
+    var count = $(this).val().replace(/\n/g, "改行").length;
+    var now_count = max_text - count;
+  
+    if (count > max_text) {
       $(".js-text-count").css("color","red");
     } else {
       $(".js-text-count").css("color","black");
