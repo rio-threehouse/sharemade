3544fee58719c7aa3227555f04d42e76181d39d6
Rio Sanka <threehouse.second@gmail.com>
2019-10-08 11:31:02 +0900

    通知デザイン 新しい通知の背景を青にする

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,10 +1,15 @@
 class NotificationsController < ApplicationController
   def index
     # result_idがcurrent_userの作った作品のidと一緒のものを取り出す
-    @notifications = Notification.where(result_id: current_user.results.select(:id)).order('created_at DESC')
-    @notifications.where(check: 'false').each do |notification|
-      notification.update(check: 'true')
+    current_user_notifications = Notification.where(result_id: current_user.results.select(:id)).order('created_at DESC')
+    @uncheck_notifications = current_user_notifications.where(check: false)
+    @check_notifications = current_user_notifications.where(check: true).limit(10)
+    
+    @uncheck_notifications.each do |notification|
+      notification.update(check: true)
     end
+    
+
   end
 end
  
