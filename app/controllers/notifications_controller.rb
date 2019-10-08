979dd63d14851e1a3408e6a4220b3d5bb00ff9b8
Rio Sanka <threehouse.second@gmail.com>
2019-10-08 16:55:20 +0900

    通知 修正 updateをactionの後にする

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,15 +1,17 @@
 class NotificationsController < ApplicationController
+  after_action :check_update
   def index
     # result_idがcurrent_userの作った作品のidと一緒のものを取り出す
     current_user_notifications = Notification.where(result_id: current_user.results.select(:id)).order('created_at DESC')
-    @uncheck_notifications = current_user_notifications.where(check: false)
     @check_notifications = current_user_notifications.where(check: true).limit(10)
-    
+    @uncheck_notifications = current_user_notifications.where(check: false)
+  end
+
+  private
+  def check_update
     @uncheck_notifications.each do |notification|
       notification.update(check: true)
     end
-    
-
   end
 end
  
